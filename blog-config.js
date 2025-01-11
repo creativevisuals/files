cccfb388750a33fdd62211b644e62fb02145ebcf
creativevisuals <dpa.visuals@yahoo.com>
2025-01-11 01:32:58 +0000

    blog-config.js

--- blog-config.js
+++ blog-config.js
@@ -1,5 +1,1 @@
-var monthFormat = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var noThumbnail = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj7PNmRRoR24MhxTHjF8rmmQVZbRmEN67jQnFubZa0FkFoLgH_CjSlElmEPFeXsloFM2RilWoqu1pmEA7po5UPeSgwdfgvVOfdGcAmcf-ssocj1CHX1ATMbjWqP6XGMJn6MJ5pNQ1FxPCA/s1600/nth.png";
-var postPerPage = 8;
-var commentsSystem = "blogger";
-var disqusShortname = "soratemplates";
+var monthFormat=["January","February","March","April","May","June","July","August","September","October","November","December"],noThumbnail="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj7PNmRRoR24MhxTHjF8rmmQVZbRmEN67jQnFubZa0FkFoLgH_CjSlElmEPFeXsloFM2RilWoqu1pmEA7po5UPeSgwdfgvVOfdGcAmcf-ssocj1CHX1ATMbjWqP6XGMJn6MJ5pNQ1FxPCA/s1600/nth.png",postPerPage=8,commentsSystem="blogger",disqusShortname="soratemplates";
